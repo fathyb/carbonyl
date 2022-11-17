a81f5b38a317d45a56ce05217925e1fae2a64fc9
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2022-11-17 20:16:48 +0000

    build: fix arm64 build

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,6 @@ target="$1"
 shift
 
 scripts/ninja.sh "$target"
-electron/src/electron/script/strip-binaries.py -d "electron/src/out/$target"
+electron/src/electron/script/strip-binaries.py -d "electron/src/out/$target" "$@"
 ninja -C "electron/src/out/$target" electron:electron_dist_zip
 
