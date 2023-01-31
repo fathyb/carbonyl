ebee363f27c29f74506600f6858c5634c0b44989
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2023-01-31 14:49:04 +0100

    use COLUMNS and LINES if term size is empty

--- src/output/size.rs
+++ src/output/size.rs
@@ -1,5 +1,5 @@
 use core::mem::MaybeUninit;
-use std::io;
+use std::{io, str::FromStr};
 
 use crate::gfx::Size;
 
@@ -11,13 +11,16 @@ pub fn size() -> io::Result<Size> {
             let mut size = ptr.assume_init();
 
             if size.ws_col == 0 || size.ws_row == 0 {
+                let cols = parse_var("COLUMNS").unwrap_or(80);
+                let rows = parse_var("LINES").unwrap_or(24);
+
                 eprintln!(
-                    "TIOCGWINSZ returned an empty size ({}x{}), defaulting to 80x24",
-                    size.ws_col, size.ws_row
+                    "TIOCGWINSZ returned an empty size ({}x{}), defaulting to {}x{}",
+                    size.ws_col, size.ws_row, cols, rows
                 );
 
-                size.ws_col = 80;
-                size.ws_row = 24;
+                size.ws_col = cols;
+                size.ws_row = rows;
             }
 
             Ok(Size::new(size.ws_col as u32, size.ws_row as u32))
@@ -25,4 +28,8 @@ pub fn size() -> io::Result<Size> {
             Err(io::Error::last_os_error())
         }
     }
+}
+
+fn parse_var<T: FromStr>(var: &str) -> Option<T> {
+    std::env::var(var).ok()?.parse().ok()
 }
