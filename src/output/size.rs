7046ebdfd3c1ca205721aaf6e4037651494fd2b0
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2023-01-31 05:21:46 +0100

    fix

--- src/output/size.rs
+++ src/output/size.rs
@@ -8,17 +8,19 @@ pub fn size() -> io::Result<Size> {
 
     unsafe {
         if libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, ptr.as_mut_ptr()) == 0 {
-            let size = ptr.assume_init();
+            let mut size = ptr.assume_init();
 
-            Ok({
-                if size.ws_col == 0 || !size.ws_row == 0 {
-                    eprintln!("TIOCGWINSZ returned an empty size, defaulting to 80x24");
+            if size.ws_col == 0 || size.ws_row == 0 {
+                eprintln!(
+                    "TIOCGWINSZ returned an empty size ({}x{}), defaulting to 80x24",
+                    size.ws_col, size.ws_row
+                );
 
-                    Size::new(80, 24)
-                } else {
-                    Size::new(size.ws_col as u32, size.ws_row as u32)
-                }
-            })
+                size.ws_col = 80;
+                size.ws_row = 24;
+            }
+
+            Ok(Size::new(size.ws_col as u32, size.ws_row as u32))
         } else {
             Err(io::Error::last_os_error())
         }
