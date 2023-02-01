0fb8ab925f797073edc713e42a2e3dac41e2acec
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2023-02-01 13:39:30 +0100

    add macos amd64

--- .refloat/config.js
+++ .refloat/config.js
@@ -15,22 +15,20 @@ const env = {
     },
 }
 
-export const jobs = [
-    {
-        name: 'Build (macOS/arm64)',
-        using: env,
-        agent: { tags: ['macos', 'arm64'] },
-        steps: [
-            'ccache --set-config=max_size=256G',
-            './scripts/gclient.sh sync',
-            './scripts/patches.sh apply',
-            [
-                './scripts/gn.sh',
-                'gen',
-                'out/Default',
-                `--args='import("//carbonyl/src/browser/args.gn") use_lld=false is_debug=false symbol_level=0 cc_wrapper="ccache"'`,
-            ].join(' '),
-            './scripts/build.sh Default',
-        ],
-    },
-]
+export const jobs = ['arm64', 'amd64'].map((arch) => ({
+    name: `Build (macOS/${arch})`,
+    using: env,
+    agent: { tags: ['macos', arch] },
+    steps: [
+        'ccache --set-config=max_size=256G',
+        './scripts/gclient.sh sync',
+        './scripts/patches.sh apply',
+        [
+            './scripts/gn.sh',
+            'gen',
+            'out/Default',
+            `--args='import("//carbonyl/src/browser/args.gn") use_lld=false is_debug=false symbol_level=0 cc_wrapper="ccache"'`,
+        ].join(' '),
+        './scripts/build.sh Default',
+    ],
+}))
