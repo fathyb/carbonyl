ef06ff6c52a05e36d2d33eae333329ad4b90427f
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2023-02-03 12:40:41 +0100

    fix

--- .refloat/config.js
+++ .refloat/config.js
@@ -10,7 +10,7 @@ export const jobs = [
             },
             {
                 export: {
-                    artifact: [`build/${target}/release/libcarbonyl.dylib`],
+                    artifact: `build/${target}/release/libcarbonyl.dylib`,
                 },
             },
         ],
