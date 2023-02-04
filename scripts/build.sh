dc6538618ff7a9e97a7889c33d2f4822b3c11cb2
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2023-02-04 20:11:00 +0100

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,19 @@
 
 export CARBONYL_ROOT=$(cd $(dirname -- "$0") && dirname -- "$(pwd)")
 
-source "$CARBONYL_ROOT/scripts/env.sh"
+cd $CARBONYL_ROOT
+source scripts/env.sh
 
-triple=$("$CARBONYL_ROOT/scripts/platform-triple.sh" "$2")
+triple=$(scripts/platform-triple.sh "$2")
 
 export MACOSX_DEPLOYMENT_TARGET=10.13
 cargo build --target "$triple" --release
+
+if [ -f "build/$triple/release/libcarbonyl.dylib" ]; then
+    install_name_tool \
+        -id @executable_path/libcarbonyl.dylib \
+        "build/$triple/release/libcarbonyl.dylib"
+fi
 
 cd "$CHROMIUM_SRC/out/$1"
 
