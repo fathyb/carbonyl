2ef3bb00f7eb2804810b9dd61d2aa5cb93deb511
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2023-02-04 15:25:16 +0100

    fix lib

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ source "$CARBONYL_ROOT/scripts/env.sh"
 
 triple=$("$CARBONYL_ROOT/scripts/platform-triple.sh" "$2")
 
+export MACOSX_DEPLOYMENT_TARGET=10.13
 cargo build --target "$triple" --release
 
 cd "$CHROMIUM_SRC/out/$1"
