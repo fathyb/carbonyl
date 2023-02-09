8f8a17c7eed6a2f222999707942dff8a07c986b2
Fathy Boundjadj <hey@fathy.fr>
2023-02-09 06:12:59 +0100

    fix: fix WebGL support on Linux
    Fixes #70

--- scripts/copy-binaries.sh
+++ scripts/copy-binaries.sh
@@ -27,5 +27,21 @@ cp "$src/libGLESv2.$lib_ext" "$dest"
 cp "$src"/v8_context_snapshot*.bin "$dest"
 cp "build/$triple/release/libcarbonyl.$lib_ext" "$dest"
 
+if [ "$lib_ext" == "so" ]; then
+    cp "$src/libVkICD_mock_icd.so" "$dest"
+    cp "$src/libVkLayer_khronos_validation.so" "$dest"
+    cp "$src/libvk_swiftshader.so" "$dest"
+    cp "$src/libvulkan.so.1" "$dest"
+    cp "$src/vk_swiftshader_icd.json" "$dest"
+fi
+
+cd "$dest"
+
+if [[ "$cpu" == "arm64" ]] && command -v aarch64-linux-gnu-strip; then
+    aarch64-linux-gnu-strip carbonyl *.so *.so.1
+else
+    strip carbonyl *.so *.so.1
+fi
+
 echo "Binaries copied to $dest"
 
