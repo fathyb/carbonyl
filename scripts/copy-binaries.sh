7e10ebadb3e1ca4b6679a471fe7c63e33bc873b2
Fathy Boundjadj <hey@fathy.fr>
2023-02-09 06:16:35 +0100

    remove unused binaries

--- scripts/copy-binaries.sh
+++ scripts/copy-binaries.sh
@@ -28,8 +28,6 @@ cp "$src"/v8_context_snapshot*.bin "$dest"
 cp "build/$triple/release/libcarbonyl.$lib_ext" "$dest"
 
 if [ "$lib_ext" == "so" ]; then
-    cp "$src/libVkICD_mock_icd.so" "$dest"
-    cp "$src/libVkLayer_khronos_validation.so" "$dest"
     cp "$src/libvk_swiftshader.so" "$dest"
     cp "$src/libvulkan.so.1" "$dest"
     cp "$src/vk_swiftshader_icd.json" "$dest"
