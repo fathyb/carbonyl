fffb087b7ed6f1c8ab694cf877cebdf48bc973b6
Fathy Boundjadj <hey@fathy.fr>
2023-02-10 10:08:40 +0100

    improve config

--- .refloat/config.js
+++ .refloat/config.js
@@ -1,6 +1,7 @@
 import pkg from "../package.json";
 
 const { version } = JSON.parse(pkg);
+const triple = (platform, arch) => `${archs[arch]}-${platforms[platform]}`
 const sharedLib = {
     macos: "dylib",
     linux: "so",
@@ -14,59 +15,19 @@ const archs = {
     amd64: "x86_64",
 };
 
-export const jobs = ["arm64", "amd64"]
-    .flatMap((arch) => ["macos", "linux"].map((platform) => ({ platform, arch })))
-    .map(({ platform, arch }) => {
-        const triple = `${archs[arch]}-${platforms[platform]}`;
-        const lib = `build/${triple}/release/libcarbonyl.${sharedLib[platform]}`;
-
-        return { platform, arch, triple, lib };
-    })
-    .flatMap(({ platform, arch, triple, lib }) => [
+export const jobs = ["macos", "linux"].flatMap(platform => {
+    return [
         {
-            name: `Build core (${platform}/${arch})`,
-            docker:
-                platform === "linux"
-                    ? {
-                        image: "fathyb/rust-cross",
-                        cache: ["/usr/local/cargo/registry"],
-                    }
-                    : undefined,
-            agent: { tags: platform === "linux" ? ["docker"] : ["macos"] },
-            steps: [
-                {
-                    name: "Install Rust toolchain",
-                    command: `rustup target add ${triple}`,
-                },
-                {
-                    name: "Build core library",
-                    command: `cargo build --target ${triple} --release`,
-                    env: { MACOSX_DEPLOYMENT_TARGET: "10.13" },
-                },
-                {
-                    name: "Set core library install name",
-                    command:
-                        platform === "macos"
-                            ? `install_name_tool -id @executable_path/libcarbonyl.dylib ${lib}`
-                            : "echo not necessary",
-                },
-                {
-                    export: {
-                        workspace: `core-${triple}`,
-                        path: "build/*/release/*.{dylib,so,dll}",
-                    },
-                },
-            ],
-        },
-        {
-            name: `Build runtime (${platform}/${arch})`,
+            name: `Build runtime (${platform})`,
             agent: { tags: ["chromium-src", platform] },
             steps: [
                 {
-                    import: { workspace: `core-${triple}` },
+                    parallel: ['arm64', 'amd64'].map(arch => (
+                        { import: { workspace: `core-${triple(platform, arch)}` } }
+                    ))
                 },
                 {
-                    name: 'Build if needed',
+                    name: 'Fetch Chromium',
                     command: `
                         if [ -z "$CHROMIUM_ROOT" ]; then
                             echo "Chromium build environment not setup"
@@ -74,7 +35,14 @@ export const jobs = ["arm64", "amd64"]
                             exit 2
                         fi
 
-                        if ! scripts/runtime-pull.sh ${arch}; then
+                        if scripts/runtime-pull.sh arm64; then
+                            touch skip-build-arm64
+                        fi
+                        if scripts/runtime-pull.sh amd64; then
+                            touch skip-build-amd64
+                        fi
+
+                        if [ ! -f skip-build-amd64 ] || [ ! -f skip-build-amd64 ]; then
                             cp chromium/.gclient "$CHROMIUM_ROOT"
 
                             scripts/gclient.sh sync
@@ -84,47 +52,119 @@ export const jobs = ["arm64", "amd64"]
                             mkdir "$CHROMIUM_ROOT/src/carbonyl"
                             ln -s "$(pwd)/src" "$CHROMIUM_ROOT/src/carbonyl/src"
                             ln -s "$(pwd)/build" "$CHROMIUM_ROOT/src/carbonyl/build"
-
-                            scripts/build.sh ${arch}
-                            scripts/copy-binaries.sh ${arch} ${arch}
                         fi
                     `
                 },
                 {
-                    export: {
-                        workspace: `runtime-${triple}`,
-                        path: `build/pre-built/${triple}`
-                    }
+                    parallel: ['arm64', 'amd64'].map(arch => ({
+                        serial: [
+                            {
+                                name: 'Build Chromium',
+                                command: `
+                                    if [ ! -f skip-build-${arch} ]; then
+                                        scripts/build.sh ${arch}
+                                        scripts/copy-binaries.sh ${arch} ${arch}
+                                    fi
+                                `
+                            },
+
+                            {
+                                parallel: [
+                                    {
+                                        name: 'Push binaries to CDN',
+                                        command: `
+                                            if [ ! -f skip-build-${arch} ]; then
+                                                scripts/runtime-push.sh ${arch}
+                                            fi
+                                        `,
+                                        env: {
+                                            CDN_ACCESS_KEY_ID: { secret: true },
+                                            CDN_SECRET_ACCESS_KEY: { secret: true }
+                                        }
+                                    },
+                                    {
+                                        export: {
+                                            workspace: `runtime-${triple(platform, arch)}`,
+                                            path: `build/pre-built/${triple(platform, arch)}`
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }))
                 }
             ],
         },
-        {
-            // TODO: setup shared build dir
-            name: `Package (${platform}/${arch})`,
-            docker: "fathyb/rust-cross",
-            agent: { tags: ["docker"] },
-            steps: [
-                {
-                    import: { workspace: `runtime-${triple}` },
-                },
-                {
-                    name: "Zip binaries",
-                    command: `
-                        mkdir build/zip
-                        cp -r build/pre-built/${triple} build/zip/carbonyl-${version}
+        ...['arm64', 'amd64'].flatMap((arch) => {
+            const triple = `${archs[arch]}-${platforms[platform]}`;
+            const lib = `build/${triple}/release/libcarbonyl.${sharedLib[platform]}`;
 
-                        cd build/zip
-                        zip -r package.zip carbonyl-${version}
-                    `,
+            return [
+                {
+                    name: `Build core (${platform}/${arch})`,
+                    docker:
+                        platform === "linux"
+                            ? {
+                                image: "fathyb/rust-cross",
+                                cache: ["/usr/local/cargo/registry"],
+                            }
+                            : undefined,
+                    agent: { tags: platform === "linux" ? ["docker"] : ["macos"] },
+                    steps: [
+                        {
+                            name: "Install Rust toolchain",
+                            command: `rustup target add ${triple}`,
+                        },
+                        {
+                            name: "Build core library",
+                            command: `cargo build --target ${triple} --release`,
+                            env: { MACOSX_DEPLOYMENT_TARGET: "10.13" },
+                        },
+                        {
+                            name: "Set core library install name",
+                            command:
+                                platform === "macos"
+                                    ? `install_name_tool -id @executable_path/libcarbonyl.dylib ${lib}`
+                                    : "echo not necessary",
+                        },
+                        {
+                            export: {
+                                workspace: `core-${triple}`,
+                                path: "build/*/release/*.{dylib,so,dll}",
+                            },
+                        },
+                    ],
                 },
                 {
-                    export: {
-                        artifact: {
-                            name: `carbonyl.${platform}-${arch}.zip`,
-                            path: "build/zip/package.zip",
+                    // TODO: setup shared build dir
+                    name: `Package (${platform}/${arch})`,
+                    docker: "fathyb/rust-cross",
+                    agent: { tags: ["docker"] },
+                    steps: [
+                        {
+                            import: { workspace: `runtime-${triple}` },
                         },
-                    },
+                        {
+                            name: "Zip binaries",
+                            command: `
+                            mkdir build/zip
+                            cp -r build/pre-built/${triple} build/zip/carbonyl-${version}
+    
+                            cd build/zip
+                            zip -r package.zip carbonyl-${version}
+                        `,
+                        },
+                        {
+                            export: {
+                                artifact: {
+                                    name: `carbonyl.${platform}-${arch}.zip`,
+                                    path: "build/zip/package.zip",
+                                },
+                            },
+                        },
+                    ],
                 },
-            ],
-        },
-    ]);
+            ]
+        })
+    ]
+})
