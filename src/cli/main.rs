c3bcb38fcd9b5f9cedfcf071e551bedcd1896642
Fathy Boundjadj <hey@fathy.fr>
2023-02-10 08:01:57 +0100

    feat(cli): add logo and description to `--help`

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -4,7 +4,7 @@ pub fn main() -> Option<CommandLine> {
     match CommandLineProgram::parse() {
         CommandLineProgram::Main(cmd) => return Some(cmd),
         CommandLineProgram::Help => {
-            println!("{}", help())
+            println!("{}", include_str!("usage.txt"))
         }
         CommandLineProgram::Version => {
             println!("Carbonyl {}", env!("CARGO_PKG_VERSION"))
@@ -12,14 +12,4 @@ pub fn main() -> Option<CommandLine> {
     }
 
     None
-}
-
-pub fn help() -> &'static str {
-    "Usage: carbonyl [options] [url]
-
-Options:
-    -h, --help                 display this help message
-    -d, --debug                enable debug logs
-    -v, --version              output the version number
-"
 }
