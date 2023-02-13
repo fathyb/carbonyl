3b9c7615d63038e48f081ce79d2ec8dd038459cb
Fathy Boundjadj <hey@fathy.fr>
2023-02-13 16:43:39 +0100

    fix linux build

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -8,6 +8,7 @@ cd "$CHROMIUM_SRC"
 
 chromium_upstream="92da8189788b1b373cbd3348f73d695dfdc521b6"
 skia_upstream="486deb23bc2a4d3d09c66fef52c2ad64d8b4f761"
+webrtc_upstream="727080cbacd58a2f303ed8a03f0264fe1493e47a"
 
 if [[ "$1" == "apply" ]]; then
     echo "Stashing Chromium changes.."
@@ -20,7 +21,7 @@ if [[ "$1" == "apply" ]]; then
     "$CARBONYL_ROOT/scripts/restore-mtime.sh" "$chromium_upstream"
 
     echo "Stashing Skia changes.."
-    cd third_party/skia
+    cd "$CHROMIUM_SRC/third_party/skia"
     git add -A .
     git stash
 
@@ -29,6 +30,16 @@ if [[ "$1" == "apply" ]]; then
     git am --committer-date-is-author-date "$CARBONYL_ROOT/chromium/patches/skia"/*
     "$CARBONYL_ROOT/scripts/restore-mtime.sh" "$skia_upstream"
 
+    echo "Stashing WebRTC changes.."
+    cd "$CHROMIUM_SRC/third_party/webrtc"
+    git add -A .
+    git stash
+
+    echo "Applying WebRTC patches.."
+    git checkout "$webrtc_upstream"
+    git am --committer-date-is-author-date "$CARBONYL_ROOT/chromium/patches/webrtc"/*
+    "$CARBONYL_ROOT/scripts/restore-mtime.sh" "$webrtc_upstream"
+
     echo "Patches successfully applied"
 elif [[ "$1" == "save" ]]; then
     if [[ -d carbonyl ]]; then
@@ -36,11 +47,15 @@ elif [[ "$1" == "save" ]]; then
     fi
 
     echo "Updating Chromium patches.."
-    git format-patch --output-directory "$CARBONYL_ROOT/chromium/patches/chromium" "$chromium_upstream"
+    git format-patch --no-signature --output-directory "$CARBONYL_ROOT/chromium/patches/chromium" "$chromium_upstream"
 
     echo "Updating Skia patches.."
-    cd third_party/skia
-    git format-patch --output-directory "$CARBONYL_ROOT/chromium/patches/skia" "$skia_upstream"
+    cd "$CHROMIUM_SRC/third_party/skia"
+    git format-patch --no-signature --output-directory "$CARBONYL_ROOT/chromium/patches/skia" "$skia_upstream"
+
+    echo "Updating WebRTC patches.."
+    cd "$CHROMIUM_SRC/third_party/webrtc"
+    git format-patch --no-signature --output-directory "$CARBONYL_ROOT/chromium/patches/webrtc" "$webrtc_upstream"
 
     echo "Patches successfully updated"
 else
