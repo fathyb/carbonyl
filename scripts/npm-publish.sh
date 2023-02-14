336bd54bf7ed4650d20772198f1701d733d51af2
Fathy Boundjadj <hey@fathy.fr>
2023-02-14 05:35:47 +0100

    fix npm publish

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -5,9 +5,6 @@ export CARBONYL_ROOT=$(cd $(dirname -- "$0") && dirname -- $(pwd))
 cd "$CARBONYL_ROOT"
 source "scripts/env.sh"
 
-platform="$1"; shift
-arch="$1"; shift
-
 cd "build/packages"
 
 if [ -z "$CARBONYL_PUBLISH_PLATFORM" ] && [ -z "$CARBONYL_PUBLISH_ARCH" ]; then
