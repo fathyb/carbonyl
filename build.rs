cfd5f9c6c75a4f87344b30b6b922ccd473b82aac
Stephen Seo <seo.disparate@gmail.com>
2023-02-25 22:31:48 +0900

    Warn if sysroot bundled by chromium doesn't exist

--- build.rs
+++ build.rs
@@ -1,21 +1,37 @@
+use std::path::PathBuf;
+
 #[cfg(target_arch = "x86_64")]
 fn link_sysroot() {
-    println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_amd64-sysroot/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_amd64-sysroot/usr/lib/x86_64-linux-gnu");
+    let sysroot_path = PathBuf::from("./chromium/src/build/linux/debian_bullseye_amd64-sysroot");
 
-    println!(
-        "cargo:rustc-link-arg=--sysroot=./chromium/src/build/linux/debian_bullseye_amd64-sysroot"
-    );
+    if sysroot_path.is_dir() {
+        println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_amd64-sysroot/lib/x86_64-linux-gnu");
+        println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_amd64-sysroot/usr/lib/x86_64-linux-gnu");
+
+        println!(
+            "cargo:rustc-link-arg=--sysroot=./chromium/src/build/linux/debian_bullseye_amd64-sysroot"
+        );
+    } else {
+        println!("cargo:warning={}", "x86_64 debian sysroot provided by chromium was not found!");
+        println!("cargo:warning={}", "carbonyl may fail to link against a proper libc!");
+    }
 }
 
 #[cfg(target_arch = "x86")]
 fn link_sysroot() {
-    println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_i386-sysroot/lib/i386-linux-gnu");
-    println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_i386-sysroot/usr/lib/i386-linux-gnu");
+    let sysroot_path = PathBuf::from("./chromium/src/build/linux/debian_bullseye_i386-sysroot");
 
-    println!(
-        "cargo:rustc-link-arg=--sysroot=./chromium/src/build/linux/debian_bullseye_i386-sysroot"
-    );
+    if sysroot_path.is_dir() {
+        println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_i386-sysroot/lib/i386-linux-gnu");
+        println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_i386-sysroot/usr/lib/i386-linux-gnu");
+
+        println!(
+            "cargo:rustc-link-arg=--sysroot=./chromium/src/build/linux/debian_bullseye_i386-sysroot"
+        );
+    } else {
+        println!("cargo:warning={}", "x86 debian sysroot provided by chromium was not found!");
+        println!("cargo:warning={}", "carbonyl may fail to link against a proper libc!");
+    }
 }
 
 #[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
