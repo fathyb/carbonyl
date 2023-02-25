655668a64d0985f9f04681acb93336be9c9517d7
Stephen Seo <seo.disparate@gmail.com>
2023-02-25 17:46:20 +0900

    Conditionally set sysroot if x86 or x86_64
    This is so that the carbonyl Rust lib links against the "debian sysroot"
    libc packaged with chromium for x86 and x86_64 builds.

--- build.rs
+++ build.rs
@@ -1,8 +1,28 @@
-fn main() {
+#[cfg(target_arch = "x86_64")]
+fn link_sysroot() {
     println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_amd64-sysroot/lib/x86_64-linux-gnu");
     println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_amd64-sysroot/usr/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_amd64-sysroot/usr");
-    println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_amd64-sysroot");
 
-    println!("cargo:rustc-link-arg=--sysroot=./chromium/src/build/linux/debian_bullseye_amd64-sysroot");
+    println!(
+        "cargo:rustc-link-arg=--sysroot=./chromium/src/build/linux/debian_bullseye_amd64-sysroot"
+    );
+}
+
+#[cfg(target_arch = "x86")]
+fn link_sysroot() {
+    println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_i386-sysroot/lib/i386-linux-gnu");
+    println!("cargo:rustc-link-search=chromium/src/build/linux/debian_bullseye_i386-sysroot/usr/lib/i386-linux-gnu");
+
+    println!(
+        "cargo:rustc-link-arg=--sysroot=./chromium/src/build/linux/debian_bullseye_i386-sysroot"
+    );
+}
+
+#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
+fn link_sysroot() {
+    // Intentionally left blank.
+}
+
+fn main() {
+    link_sysroot();
 }
